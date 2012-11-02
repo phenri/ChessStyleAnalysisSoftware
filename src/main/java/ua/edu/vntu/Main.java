0ac9bf660497ae9a7ab06ef810ee7222bd6a0b83
unknown <slavik016@gmail.com>
2012-11-02 22:06:32 +0200

    inital commit

---
+++ src/main/java/ua/edu/vntu/Main.java
@@ -0,0 +1,13 @@
+package ua.edu.vntu;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )throws InterruptedException{
+        System.out.println( "Hello World!" );
+        Thread.sleep(10000);
+    }
+}
