818bfc08143e8fbf211c757d0474cec6e9e90f74
Slavik Bychkovskyy <slavik016@gmail.com>
2012-11-02 23:36:39 +0200

    Added read file

--- src/main/java/ua/edu/vntu/Main.java
+++ src/main/java/ua/edu/vntu/Main.java
@@ -4,10 +4,10 @@ package ua.edu.vntu;
  * Hello world!
  *
  */
-public class Main
-{
-    public static void main( String[] args )throws InterruptedException{
-        System.out.println( "Hello World!" );
-        Thread.sleep(10000);
+public class Main{
+    public static void main( String[] args ){
+        new Parser();
+
+
     }
 }
---
+++ src/main/java/ua/edu/vntu/Parser.java
@@ -0,0 +1,47 @@
+package ua.edu.vntu;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slavik
+ * Date: 02.11.12
+ * Time: 22:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parser  {
+    private Map<String,String> white,black;
+    private String event, site, date,round,playerWhite,playerBlack,result;
+    public Parser(){
+        readPGN();
+    }
+    private void readPGN(){
+        try{
+            FileInputStream file = new FileInputStream("tmp\\file.pgn"); //you are must create this file, or write new path to file
+            int c, i = 0;
+            char[] chars = new char[file.available()];
+            while(( c = file.read())!= -1){
+                chars[i] = (char) c;
+                i++;
+            }
+            String content = new String(chars);
+            parseString(content,"\n");
+//            System.out.println(content);
+        }catch (IOException e){}
+    }
+
+    private void parseString(String s, String split){
+        String s1[] =  s.split(split);
+
+//        for (int i = 0; i < s1.length; i++){
+            System.out.println(s1[0]);
+
+//        }
+
+    }
+}
