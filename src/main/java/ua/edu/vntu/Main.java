ab203b473ed2e6c0a0cf07166aa6eded79fa1683
Vyacheslav Bychkovskyy <slavik016@gmail.com>
2012-11-07 13:06:17 +0200

    Fix state

--- src/main/java/ua/edu/vntu/Main.java
+++ src/main/java/ua/edu/vntu/Main.java
@@ -1,9 +1,11 @@
 package ua.edu.vntu;
 
+import ua.edu.vntu.gui.Form;
 import ua.edu.vntu.readparty.Parser;
 
 public class Main{
     public static void main( String[] args ){
         new Parser("tmp\\file.pgn");
+        new Form();
     }
 }
--- src/main/java/ua/edu/vntu/gui/chessboard/Chessboard.java
+++ src/main/java/ua/edu/vntu/gui/chessboard/Chessboard.java
@@ -21,56 +21,6 @@ public class Chessboard extends JPanel implements FormConstants{
         int start = 30;
         int x = start, y = start, c = 0;
 
-
-        for (int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                cells[i][j] = new Cell();
-                if (c % 2 == 0){
-                    cells[i][j].setBackground(lIGHT);
-                }
-                else {
-                    cells[i][j].setBackground(DARK);
-                }
-                cells[i][j].setBounds(x,y,CELL_SIZE,CELL_SIZE);
-                add(cells[i][j]);
-                x +=CELL_SIZE;
-                c++;
-            }
-            y+=CELL_SIZE;
-            x = start;
-            if (i%2 == 1 ){
-                c = 0;
-            }else {
-                c = 1;
-            }
-        }
-
-        y = 45;
-        for(int i = 1; i <= 8; i++){
-            JLabel leftVertical = new JLabel(i+"");
-            JLabel rightVertical = new JLabel(i+"");
-            leftVertical.setBounds(10, y, 20, 20);
-            rightVertical.setBounds(520, y, 20, 20);
-            add(leftVertical);
-            add(rightVertical);
-            y += CELL_SIZE;
-        }
-
-        x = 50;
-        for(int i = 'A'; i <= 'H'; i++){
-            JLabel leftVertical = new JLabel((char)i+"");
-            JLabel rightVertical = new JLabel((char)i+"");
-            leftVertical.setBounds(x, 10, 20, 20);
-            rightVertical.setBounds(x, 510, 20, 20);
-            add(leftVertical);
-            add(rightVertical);
-            x += CELL_SIZE;
-        }
-
-        setBounds(30,30,540,540);
-        setBackground(new Color(255,255, 255));
-
-
 //        for (int i = 0; i < 8; i++){
 //            for(int j = 0; j < 8; j++){
 //                cells[i][j] = new Cell();
